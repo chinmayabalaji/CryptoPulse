925abd8deb5dec00bdaf983e36e2bf8d335b4e71
Chinmaya-Balaji <chinmayabalaji03@gmail.com>
2026-08-17 23:09:23 +0530

    Adding lambda function using Terraform

--- Infrastructure/Terraform/main.tf
+++ Infrastructure/Terraform/main.tf
@@ -1,23 +1,29 @@
-terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.92"
-      }
-    }
+variable "aws_region" {
+  description = "Single AWS region used for all Terraform-managed resources."
+  type        = string
+  default     = "eu-north-1"
+}
 
-    required_version = ">= 1.2"
-    
-    backend "s3" {
-    bucket = "aws-terraform-infra-backend"
-    key = "terraform.tfstate"
-    region = "eu-north-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+
+  backend "s3" {
+    bucket       = "aws-terraform-infra-backend"
+    key          = "terraform.tfstate"
+    region       = "eu-north-1"
     use_lockfile = true
-    encrypt = true
+    encrypt      = true
   }
 }
 
 provider "aws" {
-    region = "eu-north-1"
+  region = var.aws_region
 }
 
