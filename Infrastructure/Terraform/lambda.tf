1c0dc789f5c40a3497e0e32b3ad858cdfa657c9d
Chinmaya-Balaji <chinmayabalaji03@gmail.com>
2026-08-17 22:46:16 +0530

    Adding lambda function using Terraform

--- Infrastructure/Terraform/lambda.tf
+++ Infrastructure/Terraform/lambda.tf
@@ -2,6 +2,6 @@ resource "aws_lambda_function" "lambda_function" {
     filename = "../../Data_Extraction/lambda_function.py"
     function_name = "Crypto_Lambda_Function"
     role = aws_iam_role.crypto_role.arn
-    runtime = "python3.7"
+    runtime = "python3.12"
     handler = "lambda_function.lambda_handler"
 }
