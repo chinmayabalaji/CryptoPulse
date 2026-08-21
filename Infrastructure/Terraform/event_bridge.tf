8f03b1ebe14e0f4105e57ce01d269048ecf5aec2
Chinmaya-Balaji <chinmayabalaji03@gmail.com>
2026-08-21 10:14:45 +0530

    Adding Eventbridge scheduler using Terraform

--- Infrastructure/Terraform/event_bridge.tf
+++ Infrastructure/Terraform/event_bridge.tf
@@ -8,7 +8,7 @@ resource "aws_scheduler_schedule" "Crypto-Lambda-Scheduler" {
   schedule_expression = "rate(10 minutes)"
 
   target{
-    arn = aws_lambda_function.crypto_lambda.arn
-    role_arn = aws_iam_role.lambda_scheduler_role.arn
+    arn = aws_lambda_function.lambda_function.arn
+    role_arn = aws_iam_role.crypto_role.arn
   }
 }
